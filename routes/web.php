d47ae82f77b7134c5c703e921084f340ae2f5b5b
Bruno <moyse94@gmail.com>
2022-12-30 20:03:08 +0100

    remove /api

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::post('/payReminder/{id}', 'App\Http\Controllers\MessageController@payReminder');
+
+Route::prefix('contacts')->group(function () {
+    Route::get('/', 'App\Http\Controllers\ContactController@index');
+
+    Route::get('/{id}', 'App\Http\Controllers\ContactController@show');
+
+    Route::post('/', 'App\Http\Controllers\ContactController@create');
+
+    Route::put('/{id}', 'App\Http\Controllers\ContactController@update');
+
+    Route::delete('/{id}', 'App\Http\Controllers\ContactController@delete');
 });
